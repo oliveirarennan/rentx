50b2d7b9b599bb7a7ab7e4b10c74384d89562d19
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-08-16 15:33:51 -0300

    feat: Add Jest

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoryRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+export default class CategoryRepositoryInMemory implements ICategoryRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    const list = this.categories;
+
+    return list;
+  }
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,45 @@
+import AppError from "../../../../errors/AppError";
+import CategoryRepositoryInMemory from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Create  Category", () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoryRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoryRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a category", async () => {
+    const fakeCategory = {
+      name: "Fake Category",
+      description: "Fake Category description",
+    };
+
+    await createCategoryUseCase.execute(fakeCategory);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      fakeCategory.name
+    );
+
+    console.log(categoryCreated);
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with the same name", async () => {
+    expect(async () => {
+      const fakeCategory = {
+        name: "Fake Category",
+        description: "Fake Category description",
+      };
+
+      await createCategoryUseCase.execute(fakeCategory);
+
+      await createCategoryUseCase.execute(fakeCategory);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
